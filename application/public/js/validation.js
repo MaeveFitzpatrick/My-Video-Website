bacf8cfbcf5ddbdf781ebe2176d6da91ecd05590
maeve-fitzpatrick <mfitzpatrick@sfsu.edu>
2023-07-12 15:48:47 -0700

    adjusted registration.html to accomodate event listener.

--- application/public/js/validation.js
+++ application/public/js/validation.js
@@ -1,1 +1,15 @@
-console.log("HELLO");
+// //variable that gives all elements named body
+// var bodies = document.getElementsByTagName('body');
+
+console.log('HELLO');
+
+
+document.getElementById('username').addEventListener('input', function(ev){
+    var userInput = ev.target;
+    var username = userInput.value;
+    if(username.length < 3){
+        console.log('invalid username');
+    } else {
+        console.log('valid username');
+    }
+})
